82a116fc9f8c7c8e2cd47fdd43ab99d642dbc107
Johnny Slack <1318041+johnny-cartelle@users.noreply.github.com>
2023-04-20 08:13:20 -0400

    fs video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<iframe 
+    class="video"
+    width="100%" 
+    height="100%" 
+    src="https://www.youtube.com/embed/l2GmatlqnsE" 
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    allowfullscreen></iframe>
+
+<style lang="scss">
+    .video {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
